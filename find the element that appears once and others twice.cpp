d0d33760e3619b5a767dc38349b29664e0ac355f
Sanjay Datt Joshi <sanjayjoshi7409882635@gmail.com>
2025-10-28 04:23:16 +0530

    Update find the element that appears once and others twice.cpp

--- find the element that appears once and others twice.cpp
+++ find the element that appears once and others twice.cpp
@@ -1,6 +1,6 @@
 // Problem - 13 --> Find the elements that appears once and others twice.
 
-// BruteForce Approach
+// BruteForce Approach --> it will take O(n*n) TC.
 int appear_once(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -15,4 +15,28 @@ int appear_once(int arr[], int n)
         if (count == 1)
             return num;
     }
+}
+
+// Better Approach -->if m = n/2+1 is size of the map
+// it takes O(nlogm) + O(n/2+1) TC and O(n/2+1) SC.
+int appear_once(int arr[], int n)
+{
+   map<int, int> mymp;
+
+   for(int i=0; i<n; i++){
+    mymp[arr[i]]++;
+   }
+   for(auto it:mymp){
+    if(it.second == 1) return it.first;
+   }
+}
+
+// Optimal Approach --> it will take O(n) TC.
+int appear_once(int arr[], int n)
+{
+   int XOR =0;
+   for(int i=0; i<n; i++){
+    XOR = XOR ^ arr[i];
+   }
+   return XOR;
 }
